10be2d51a0aa3af6fc9ecfd8ee31efeb8c672ac4
Binay Dahal <bidahal@dwnp.deerwalk.com>
2016-02-08 18:30:09 +0545

    first push to new repo

---
+++ dropdown.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Drop Down List</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="styled-select">
+            <div class="inner-addon right-addon">
+                <select class="form-control dropdown-select">
+                    <option>Here is the first option</option>
+                    <option>The second option</option>
+                </select>
+                <i class="fa fa-caret-down dropdown-icon"></i>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Custom Components</title>
+    </head>
+    <body>
+        <ul>
+            <li>Custom Components for HTML Using Bootstrap</li>
+            <li><a href="dropdown.php">Drop Down</a></li>
+        </ul>
+        
+    </body>
+</html>
